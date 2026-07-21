166455a82153e3192087f3d2a1df24411801c22b
Michael Oluseyi Jesubunmi <michaelJesubunmi01@gmail.com>
2026-07-21 04:30:37 +0100

    Add Google Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,13 +25,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} antialiased bg-slate-50`}>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-slate-50`}
+      >
+        <GoogleAnalytics />
         {children}
       </body>
-      <body>
-    <GoogleAnalytics />
-    {children}
-</body>
     </html>
   );
 }
